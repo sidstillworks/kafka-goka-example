6fbd29c472f13346400a90ca3498f04e97293f48
sidstillworks <55650832+sidstillworks@users.noreply.github.com>
2022-07-07 23:22:43 +0530

    Avro inclusion for encoding and decoding

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/linkedin/goavro"
 	"github.com/lovoo/goka"
-	"github.com/lovoo/goka/codec"
 )
 
 var (
@@ -18,14 +18,15 @@ var (
 	topic   goka.Stream = "random-no-upto-five"
 	group   goka.Group  = "mini-random-group-upto-five"
 
-	tmc *goka.TopicManagerConfig
+	tmc       *goka.TopicManagerConfig
+	avrocodec *goavro.Codec
 )
 
 // A user is the object that is stored in the processor's group table
 type user struct {
 	EarlierTime string
 	LatestTime  string
-	TotalValue  int64
+	TotalValue  int32
 }
 
 // This codec allows marshalling (encode) and unmarshalling (decode) the user to and from the
@@ -38,49 +39,69 @@ func init() {
 	tmc.Stream.Replication = 1
 }
 
+func serialiseMap(value interface{}) map[string]interface{} {
+	userMap := make(map[string]interface{})
+
+	userMap["EarlierTime"] = value.(*user).EarlierTime
+	userMap["LatestTime"] = value.(*user).LatestTime
+	userMap["TotalValue"] = value.(*user).TotalValue
+
+	return userMap
+}
+
+func deSerialiseMap(value interface{}) user {
+	var newUser user
+
+	newUser.EarlierTime = value.(map[string]interface{})["EarlierTime"].(string)
+	newUser.LatestTime = value.(map[string]interface{})["LatestTime"].(string)
+	newUser.TotalValue = value.(map[string]interface{})["TotalValue"].(int32)
+
+	return newUser
+}
+
 // Encodes a user into []byte
-func (jc *userCodec) Encode(value interface{}) ([]byte, error) {
+func (codec userCodec) Encode(value interface{}) ([]byte, error) {
 	if _, isUser := value.(*user); !isUser {
 		return nil, fmt.Errorf("Codec requires value *user, got %T", value)
 	}
-	return json.Marshal(value)
+
+	userMap := serialiseMap(value)
+
+	binary, err := avrocodec.BinaryFromNative(nil, userMap)
+	if err != nil {
+		return nil, fmt.Errorf("Error marshaling user: %v", err)
+	}
+	return binary, nil
 }
 
 // Decodes a user from []byte to it's go representation.
-func (jc *userCodec) Decode(data []byte) (interface{}, error) {
-	var (
-		c   user
-		err error
-	)
-	err = json.Unmarshal(data, &c)
+func (codec userCodec) Decode(data []byte) (interface{}, error) {
+	native, _, err := avrocodec.NativeFromBinary(data)
 	if err != nil {
 		return nil, fmt.Errorf("Error unmarshaling user: %v", err)
 	}
-	return &c, nil
+
+	newUser := deSerialiseMap(native)
+	return &newUser, nil
 }
 
-func runEmitter() {
+func runEmitter(avrocodec *goavro.Codec) {
 	emitter, err := goka.NewEmitter(brokers, topic,
-		new(codec.String))
+		new(userCodec))
 	if err != nil {
 		panic(err)
 	}
 	defer emitter.Finish()
 
-	t := time.NewTicker(1 * time.Second)
+	t := time.NewTicker(5 * time.Second)
 	defer t.Stop()
 
 	var i int
 	for range t.C {
 		var u user
 		u.LatestTime = time.Now().String()
-		u.TotalValue = int64(i % 10)
+		u.TotalValue = int32(i % 10)
 		key := fmt.Sprintf("%d", i%6)
-		emitter, err := goka.NewEmitter(brokers, topic, new(userCodec))
-		if err != nil {
-			panic(err)
-		}
-		defer emitter.Finish()
 		emitter.EmitSync(key, &u)
 		i++
 	}
@@ -91,7 +112,7 @@ func process(ctx goka.Context, msg interface{}) {
 	m := msg.(*user)
 	if val := ctx.Value(); val != nil {
 		u = val.(*user)
-		if u.EarlierTime == "" {
+		if u.EarlierTime == "" || len(u.EarlierTime) <= 0 {
 			u.EarlierTime = m.LatestTime
 		}
 	} else {
@@ -102,7 +123,7 @@ func process(ctx goka.Context, msg interface{}) {
 	u.LatestTime = m.LatestTime
 	u.TotalValue += m.TotalValue
 	ctx.SetValue(u)
-	fmt.Printf("[proc] key: %s clicks: %d, msg: %v\n", ctx.Key(), u.TotalValue, msg)
+	fmt.Printf("[proc] key: %s clicks: %d, msg: %v, userTable: %v, ctxVal: %v\n", ctx.Key(), u.TotalValue, msg, u, ctx.Value())
 }
 
 func runProcessor(initialized chan struct{}) {
@@ -148,6 +169,17 @@ func runView(initialized chan struct{}) {
 }
 
 func main() {
+	avrocodec, _ = goavro.NewCodec(`
+		{
+			"type": "record",
+			"name": "User",
+			"fields": [
+				{"name": "EarlierTime", "type" : "string", "default": ""},
+				{"name": "LatestTime", "type" : "string", "default": ""},
+				{"name": "TotalValue", "type" : "int"}
+			]
+		}`)
+
 	tm, err := goka.NewTopicManager(brokers, goka.DefaultConfig(), tmc)
 	if err != nil {
 		log.Fatalf("Error creating topic manager: %v", err)
@@ -162,7 +194,7 @@ func main() {
 	// by goka.NewProcessor)
 	initialized := make(chan struct{})
 
-	go runEmitter()
+	go runEmitter(avrocodec)
 	go runProcessor(initialized)
 	runView(initialized)
 }
